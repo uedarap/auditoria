d391d4d6042a7eabb81a962c0f08927f20d25dbd
Raphael Ueda <raphael.ueda@reserve.com.br>
2023-05-24 15:41:10 -0300

    Primeiro Commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import PaginaAuditoria from './pages/Auditoria.vue'
+
+</script>
+
+<template>
+  <PaginaAuditoria></PaginaAuditoria>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Auditoria.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="q-pa-md">
+        <q-table
+            title="Auditoria"
+            :rows="rows"
+            :columns="columns"
+            row-key="name"
+        >
+        </q-table>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: "PaginaAuditoria",
+        data() {
+            return {
+                columns: [
+                    { name: 'acao-realizada', align: 'left', label: 'Ação Realizada', field: 'acao-realizada', sortable: true },
+                    { name: 'data-alteracao', align: 'left', label: 'Data de Alteração', field: 'data-alteracao', sortable: true },
+                    { name: 'id-usuario', align: 'left', label: 'Id Usuário', field: 'acao-realizada', sortable: true },
+                    { name: 'nome-usuario', align: 'left', label: 'Nome Usuário', field: 'nome-usuario', sortable: true }
+                ],
+                rows: []
+            }
+        }
+    })
+
+</script>
